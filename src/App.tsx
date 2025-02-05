f4347ad5c2bc53eb12b81b7c01f9ea495510e9a8
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-05 17:57:11 +0100

    fått mina states till komponenten i inspekteraläget

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,33 @@ function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
+  //call function fetchData with useEffect
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
   const fetchData = async () => {
     try {
       setLoading(true); //loading when fetching data
 
       const res = await fetch('https://m2-api-1.onrender.com/task')
+
+      if (!res.ok) {
+        throw Error('Failure' + res.status);
+      }
+
+      const data = await res.json();
+
+      setTodos(data);
+
     } catch (error) {
-      
+
+      setError('Failure fetching todos');
+
+    } finally {
+
+      setLoading(false);
+
     }
   }
 
