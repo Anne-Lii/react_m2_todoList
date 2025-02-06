b4d687229d2fd45fc6045c791ca82c5c244b053d
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-06 13:59:41 +0100

    lagt till styling och font

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,13 @@
-
+import './Todo.css'
 
 const Todo = ({todo} : {todo: any}) => {
-  
+
   //colorchange on status
-  const statusColor = todo.status ==='Ej påbörjad' ? 'red' :todo.status ==='Påbörjad' ? 'orange' : 'green'
+  const statusColor = todo.status ==='Ej påbörjad' ? 'rgb(252, 128, 128)' :todo.status ==='Påbörjad' ? 'rgb(230, 173, 87)' : 'rgb(106, 236, 113)'
 
   return (
     <section>
-        <h2>{todo.title}</h2>
+        <h3>{todo.title}</h3>
         <p>{todo.description}</p>
         <p style={{color : statusColor}}><strong>{todo.status}</strong></p>
     </section>
